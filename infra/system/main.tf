c1dbc312b69e8637b2226ae4aebe97de1cfbf0e6
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-03-29 11:48:12 +1100

    Increased memory size as 128MB is sometimes not enough

--- infra/system/main.tf
+++ infra/system/main.tf
@@ -81,7 +81,7 @@ resource "google_cloudfunctions_function" "function" {
   runtime     = "nodejs10"
 
   service_account_email = google_service_account.service_account.email
-  available_memory_mb   = 128
+  available_memory_mb   = 256
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
   trigger_http          = true
