5e19caa5b9e9a6e0b0e53c6566bc5b7df2bac35e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-03 22:06:22 +1100

    Added tests around nested objects

--- conversation-history/tests/test_conversation_history.py
+++ conversation-history/tests/test_conversation_history.py
@@ -9,6 +9,10 @@ def test_number():
     assert parser.parse('4') == 4
 
 
+def test_object_empty():
+    assert parser.parse('{}') == {}
+
+
 def test_object_without_brackets():
     assert parser.parse('first_name: "foo"') == {'first_name': 'foo'}
 
@@ -17,11 +21,21 @@ def test_object_with_brackets():
     assert parser.parse('{first_name: "foo"}') == {'first_name': 'foo'}
 
 
+def test_object_nested():
+    assert parser.parse('person {first_name: "foo"}') == {'person': {'first_name': 'foo'}}
+
+
+def test_object_nested_3_levels():
+    assert parser.parse('person {father {first_name: "foo"}}') == {'person': {'father': {'first_name': 'foo'}}}
+
+
 def test_object_multiple_keys():
     assert parser.parse("""\
         first_name: "foo"
-        last_name: "bar"
-        """) == {'first_name': 'foo', 'last_name': 'bar'}
+        father {
+          first_name: "foo-dad"
+        }
+        """) == {'first_name': 'foo', 'father': {'first_name': 'foo-dad'}}
 
 
 def test_string_with_double_quote():
